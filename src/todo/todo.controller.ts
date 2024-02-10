3f310d99a873f3b6e5961a76b6a885f291707b50
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-10 15:31:00 +0700

    dynamic route

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  @Get('/:title?')
+  getAllTodo(@Param('title')title:string = '') {
+    return `Todo Title ${title}`;
+  }
+}
