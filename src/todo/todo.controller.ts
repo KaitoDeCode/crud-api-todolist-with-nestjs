49aeac95472c59c59c79bc4d5ce7f39d213e2393
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-10 15:37:46 +0700

    http post

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {
+  
   @Get('/:title?')
   getAllTodo(@Param('title')title:string = '') {
     return `Todo Title ${title}`;
+  }
+
+  @Post()
+  createTodo(@Body() payload:any){
+    console.log(payload);
   }
 }
